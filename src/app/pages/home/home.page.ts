c9d413d7638ff3ea269e910b52a764241b2debba
Zach Brown <bbrown64506@gmail.com>
2026-04-01 17:57:36 -0500

    feat: recipe list

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import {
   IonHeader,
@@ -8,23 +8,42 @@ import {
   IonButtons,
   IonButton,
   IonIcon,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonNote,
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { logOutOutline } from 'ionicons/icons';
 import { SupabaseService } from '@app/services/supabase.service';
 import { RecipeService } from '@app/services/recipe.service';
+import { RecipeWithDetails } from '@app/models';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButtons,
+    IonButton,
+    IonIcon,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonNote,
+  ],
 })
 export class HomePage implements OnInit {
   private supabase = inject(SupabaseService);
   private recipeService = inject(RecipeService);
   private router = inject(Router);
+
+  recipes = signal<RecipeWithDetails[]>([]);
 
   constructor() {
     addIcons({ logOutOutline });
@@ -32,11 +51,15 @@ export class HomePage implements OnInit {
 
   async ngOnInit() {
     const recipes = await this.recipeService.getRecipes();
-    console.log('Recipes:', recipes);
+    this.recipes.set(recipes);
+  }
+
+  async openRecipe(id: string) {
+    await this.router.navigateByUrl(`/recipes/${id}`);
   }
 
   async logout() {
     await this.supabase.signOut();
-    this.router.navigateByUrl('/auth/login');
+    await this.router.navigateByUrl('/auth/login');
   }
 }
