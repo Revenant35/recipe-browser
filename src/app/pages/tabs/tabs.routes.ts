53c71a4e695b5bbf695e2e43378c41e32931236f
Zach Brown <bbrown64506@gmail.com>
2026-04-02 20:19:50 -0500

    refactor: organize tabs

--- src/app/pages/tabs/tabs.routes.ts
+++ src/app/pages/tabs/tabs.routes.ts
@@ -3,15 +3,15 @@ import { Routes } from '@angular/router';
 export const tabsRoutes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('../home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
     path: 'explore',
-    loadComponent: () => import('../explore/explore.page').then((m) => m.ExplorePage),
+    loadComponent: () => import('./explore/explore.page').then((m) => m.ExplorePage),
   },
   {
     path: 'settings',
-    loadComponent: () => import('../settings/settings.page').then((m) => m.SettingsPage),
+    loadComponent: () => import('./settings/settings.page').then((m) => m.SettingsPage),
   },
   {
     path: '',
